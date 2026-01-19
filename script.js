b411dc8e6101b53f06b56495af0d68e3719f3b71
Rayssa Viana <rayssavianar@gmail.com>
2026-01-19 16:45:54 -0300

    Adicionando pontos e traços para aparecerem no automático

--- script.js
+++ script.js
@@ -10,3 +10,17 @@ cpf.addEventListener("keydown", (e) => {
         button.click();
     }                   
 });
+
+cpf.addEventListener("input", () => {
+    let digits = cpf.value.replace(/\D/g, '').slice(0, 11);
+    if (digits.length > 9) {
+        cpf.value = digits.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, "$1.$2.$3-$4");
+    }   else if (digits.length > 6) {
+        cpf.value = digits.replace(/(\d{3})(\d{3})(\d{0,3})/, "$1.$2.$3");
+    } else if (digits.length > 3) {
+        cpf.value = digits.replace(/(\d{3})(\d{0,3})/, "$1.$2");
+    } else {
+        cpf.value = digits;
+    }   
+});
+
